1381d96343f9817b2215ef609dc0c7eb5b78b1ad
alcode111 <al.code.111@gmail.com>
2024-04-17 18:11:49 +0200

    First commit

---
+++ SwiftUIBBQuotes/SwiftUIBBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBBQuotesApp.swift
+//  SwiftUIBBQuotes
+//
+//  Created by Ismaïl LP on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

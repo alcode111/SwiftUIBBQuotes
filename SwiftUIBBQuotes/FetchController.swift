3b53ef2258298501fa32f62d206341018dd2a36d
alcode111 <al.code.111@gmail.com>
2024-04-18 00:49:00 +0200

    Complete fetchCharacter function

--- SwiftUIBBQuotes/FetchController.swift
+++ SwiftUIBBQuotes/FetchController.swift
@@ -39,5 +39,22 @@ struct FetchController {
         let characterURL = baseURL.appending(path: "characters")
         var characterComponents = URLComponents(url: characterURL, resolvingAgainstBaseURL: true)
         let characterQueryItem = URLQueryItem(name: "name", value: name.replaceSpaceWithPlus)
+        characterComponents?.queryItems = [characterQueryItem]
+        
+        guard let fetchURL = characterComponents?.url else {
+            throw NetworkError.badURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw NetworkError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let characters = try decoder.decode([Character].self, from: data)
+        
+        return characters[0]
     }
 }

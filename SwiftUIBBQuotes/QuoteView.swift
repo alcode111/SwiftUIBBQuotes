3baa14ad4939f32961b586e68d80680dfdccc6ca
alcode111 <al.code.111@gmail.com>
2024-04-19 12:00:06 +0200

    Add more string methods and an enum, complete QuoteView for both shows

--- SwiftUIBBQuotes/QuoteView.swift
+++ SwiftUIBBQuotes/QuoteView.swift
@@ -15,54 +15,56 @@ struct QuoteView: View {
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                Image(show.lowercased().filter { $0 != " " })
+                Image(show.lowerNoSpaces)
                     .resizable()
                     .frame(
                         width: geo.size.width * 2.7,
                         height: geo.size.height * 1.2)
                 
                 VStack {
-                    Spacer(minLength: 140)
-                    
-                    switch viewModel.status {
-                    case .success(let data):
-                        Text("\"\(data.quote.quote)\"")
-                            .minimumScaleFactor(0.5)
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.white) // So the text is visible in light mode
-                            .padding()
-                            .background(.black.opacity(0.5))
-                            .cornerRadius(25)
-                            .padding(.horizontal)
+                    VStack {
+                        Spacer(minLength: 140)
                         
-                        ZStack(alignment: .bottom) {
-                            AsyncImage(url: data.character.images[0]) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
+                        switch viewModel.status {
+                        case .success(let data):
+                            Text("\"\(data.quote.quote)\"")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.white) // So the text is visible in light mode
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .cornerRadius(25)
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: data.character.images[0]) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                                
+                                Text(data.quote.character)
+                                    .foregroundStyle(.white)
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
                             }
                             .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                            .cornerRadius(80)
                             
-                            Text(data.quote.character)
-                                .foregroundStyle(.white)
-                                .padding(10)
-                                .frame(maxWidth: .infinity)
-                                .background(.ultraThinMaterial)
+                        case .fetching:
+                            ProgressView()
+                            
+                        default:
+                            EmptyView()
                         }
-                        .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
-                        .cornerRadius(80)
                         
-                    case .fetching:
-                        ProgressView()
-                        
-                    default:
-                        EmptyView()
+                        Spacer()
                     }
                     
-                    Spacer()
-                                                            
                     Button {
                         Task {
                             await viewModel.getData(for: show)
@@ -72,9 +74,9 @@ struct QuoteView: View {
                             .font(.title)
                             .foregroundStyle(.white)
                             .padding()
-                            .background(.breakingBadGreen)
+                            .background(Color("\(show.noSpaces)Button"))
                             .cornerRadius(7)
-                            .shadow(color: .breakingBadYellow, radius: 2)
+                            .shadow(color: Color("\(show.noSpaces)Shadow"), radius: 2)
                     }
                     
                     Spacer(minLength: 180)
@@ -91,6 +93,6 @@ struct QuoteView: View {
 }
 
 #Preview {
-    QuoteView(show: "Better Call Saul")
+    QuoteView(show: Constants.bcsName)
         .preferredColorScheme(.dark)
 }
